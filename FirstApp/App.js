3e46c8e04a5e3c3e4af61045949de1d747aae2c5
Jana Safwan <22210431@students.hebron.edu>
2026-03-24 19:21:19 +0200

    update

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -1,17 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
-import Shopcard from './components/Shopcard';
-import Total from './components/Total';
+import { Button, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import Divider from './components/Divider';
+import AddStudentForm from './components/AddStudentForm';
+import StudentsList from './components/StudentsList';
+
+import { useState } from 'react';
 
 export default function App() {
+  const [studentData, setStudentData] = useState([
+    {
+      id: 1233,
+      name: "Ahmad Saeed",
+      mark: 90,
+      image: 'https://img.freepik.com/premium-psd/avatar-wearing-t-shirt-mockup_23-2151560907.jpg'
+    },
+    {
+      id: 1324,
+      name: "Sarah Jameel",
+      mark: 87,
+      image: 'https://static.vecteezy.com/system/resources/previews/055/056/329/non_2x/3d-icon-avatar-cartoon-woman-holding-a-mockup-phone-with-blank-white-screen-png.png'
+    }]
+  );
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Header itemsCount={3}/>
-      <Shopcard/>
-      <Shopcard/>
-      <Shopcard/>
-      <Total/>
+      <ImageBackground
+        source={{ uri: 'https://media.istockphoto.com/id/1064172872/vector/line-circle-abstract-background-seamless-pattern-gold-luxury-color-geometric-vector.jpg?s=612x612&w=0&k=20&c=IUfmx3Q48sDHNXSUnI-syPX1w8rol2p1--wywGddUis=' }}
+        resizeMode="repeat"
+        style={{ justifyContent: 'space-evenly', alignItems: 'center', width: '100%', paddingVertical: 20 }}
+      >
+        <Image
+          source={require('./assets/icon.png')}
+          style={{ width: 90, height: 90 }}
+          borderRadius={50}
+        />
+        <Text style={styles.title}>Student Registration App</Text>
+        <Button
+          title="Add Student ➕"
+          onPress={() => setShowForm(!showForm)}
+        />
+        {
+          showForm === true
+            ? <AddStudentForm
+              studentData={studentData}
+              setStudentData={setStudentData}
+            />
+            : null
+        }
+      </ImageBackground>
+      <Divider />
+      <StudentsList
+        studentsList={studentData}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -20,17 +61,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F87C63',
+    backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'center',
   },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#6633cc',
+    marginBottom: 20
+  }
 });
-// #FCC6BB;
-// #FAA18F;
-// #F87C63;
-// #F54927;
-// #F4320B;
-// #C82909;
-// #9C2007;
-// #701705;
-// #440E03;
-// #180501;
